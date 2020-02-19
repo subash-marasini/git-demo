8fc6eb8aff23582feac20305f60f4bae38c84290
Subash Marasini <subashyt@gmail.com>
2020-02-19 13:02:06 -0500

    v1

---
+++ src/com/marasini/gitdemo/GitDemo.java
@@ -0,0 +1,10 @@
+package com.marasini.gitdemo;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Git Demo with Eclipse");
+	}
+
+}
